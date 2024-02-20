dde6083ef26a6f5a1092d6c55fbf67b5a638edde
DavisWere <devisodhis10@gmail.com>
2024-02-20 16:55:51 +0300

    env config

--- HouseRentalApi/settings.py
+++ HouseRentalApi/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-from env import load_dotenv
+from dotenv import load_dotenv
 load_dotenv()  # loads the configs from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
